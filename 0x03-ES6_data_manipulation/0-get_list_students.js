798767dce3e0e2bbf5f13d30de0d4cd01b2e5a02
yobweezy <bkingeno07@gmail.com>
2023-02-03 06:51:33 +0000

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+const getListStudents = () => [
+	{id: 1, firstName: 'Guillaume', location: 'San Fransisco'},
+	{id: 2, firstName: 'James', location: 'Columbia'},
+	{id: 5, firstName: 'Serena', location: 'San Fransisco'}
+];
+
+export default getListStudents;
